11854e5cbf792564a61cfaa0a9b127701a25a9c6
Zoe <dadac123@gmail.com>
2015-10-27 20:56:24 +0800

    text label

---
+++ label.py
@@ -0,0 +1,10 @@
+from Tkinter import *
+# if you are working under python 3, comment the previous line and comment out the folling line
+# form tkinter import *
+
+root = Tk()
+
+w = Label(root, text='Hello Tkinter!')
+w.pack()
+
+root.mainloop()
