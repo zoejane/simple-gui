99b648ba639d12b7a1b614cc9da15c90ea57f078
Zoe <dadac123@gmail.com>
2015-10-28 20:50:37 +0800

    add padding

--- layout-pack.py
+++ layout-pack.py
@@ -3,10 +3,10 @@ from Tkinter import *
 root=Tk()
 
 w=Label(root, text='Red Sun',bg='red',fg='white')
-w.pack(fill=X)
+w.pack(fill=X,padx=10,pady=10)
 w=Label(root,text='Green Grass',bg='green',fg='black')
-w.pack(fill=X)
+w.pack(fill=X,padx=10,pady=10)
 w=Label(root,text='Blue Sky',bg='blue',fg='white')
-w.pack(fill=X)
+w.pack(fill=X,padx=10,pady=10,ipady=10)
 
 mainloop()
