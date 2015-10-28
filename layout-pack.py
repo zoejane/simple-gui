c62db0e75abc42fd45bb5b3e192aec5199587477
Zoe <dadac123@gmail.com>
2015-10-28 20:47:49 +0800

    make the widgets as wide as the parent widget

--- layout-pack.py
+++ layout-pack.py
@@ -2,8 +2,11 @@ from Tkinter import *
 
 root=Tk()
 
-Label(root, text='Red Sun',bg='red',fg='white').pack()
-Label(root,text='Green Grass',bg='green',fg='black').pack()
-Label(root,text='Blue Sky',bg='blue',fg='white').pack()
+w=Label(root, text='Red Sun',bg='red',fg='white')
+w.pack(fill=X)
+w=Label(root,text='Green Grass',bg='green',fg='black')
+w.pack(fill=X)
+w=Label(root,text='Blue Sky',bg='blue',fg='white')
+w.pack(fill=X)
 
 mainloop()
