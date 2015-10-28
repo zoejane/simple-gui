366b84c9ef79a8c1e64f7be1883df9ff7d97d0e8
Zoe <dadac123@gmail.com>
2015-10-28 20:52:25 +0800

    change side

--- layout-pack.py
+++ layout-pack.py
@@ -3,9 +3,9 @@ from Tkinter import *
 root=Tk()
 
 w=Label(root, text='Red Sun',bg='red',fg='white')
-w.pack(fill=X,padx=10,pady=10)
+w.pack(fill=X,padx=10,pady=10,side=LEFT)
 w=Label(root,text='Green Grass',bg='green',fg='black')
-w.pack(fill=X,padx=10,pady=10)
+w.pack(fill=X,padx=10,pady=10,side=RIGHT)
 w=Label(root,text='Blue Sky',bg='blue',fg='white')
 w.pack(fill=X,padx=10,pady=10,ipady=10)
 
