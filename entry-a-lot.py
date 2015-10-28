d4b1f4d8143d3c7c0fb06dedb7737f93624ef9b6
Zoe <dadac123@gmail.com>
2015-10-28 20:20:08 +0800

    entry a lot

--- entry-a-lot.py
+++ entry-a-lot.py
@@ -1,4 +1,4 @@
-'''from Tkinter import *
+from Tkinter import *
 fields='Last Name','First Name','Job','Country'
 
 def fetch(entries):
@@ -13,7 +13,7 @@ def makeform(root,fields):
 		row=Frame(root)
 		lab=Label(row,width=15,text=field,anchor='w')
 		ent=Entry(row)
-		row.pack(side=TOP,filx=X,padx=5,pady=5)
+		row.pack(side=TOP,fill=X,padx=5,pady=5)
 		lab.pack(side=LEFT)
 		ent.pack(side=RIGHT,expand=YES,fill=X)
 		entries.append((field,ent))
@@ -27,36 +27,5 @@ if __name__=='__main__':
 	b1.pack(side=LEFT,padx=5,pady=5)
 	b2=Button(root,text='Quit',command=root.quit)
 	b2.pack(side=LEFT,padx=5,pady=5)
-	root.mainloop()'''
+	root.mainloop()
 
-from Tkinter import *
-fields = 'Last Name', 'First Name', 'Job', 'Country'
-
-def fetch(entries):
-   for entry in entries:
-      field = entry[0]
-      text  = entry[1].get()
-      print('%s: "%s"' % (field, text)) 
-
-def makeform(root, fields):
-   entries = []
-   for field in fields:
-      row = Frame(root)
-      lab = Label(row, width=15, text=field, anchor='w')
-      ent = Entry(row)
-      row.pack(side=TOP, fill=X, padx=5, pady=5)
-      lab.pack(side=LEFT)
-      ent.pack(side=RIGHT, expand=YES, fill=X)
-      entries.append((field, ent))
-   return entries
-
-if __name__ == '__main__':
-   root = Tk()
-   ents = makeform(root, fields)
-   root.bind('<Return>', (lambda event, e=ents: fetch(e)))   
-   b1 = Button(root, text='Show',
-          command=(lambda e=ents: fetch(e)))
-   b1.pack(side=LEFT, padx=5, pady=5)
-   b2 = Button(root, text='Quit', command=root.quit)
-   b2.pack(side=LEFT, padx=5, pady=5)
-   root.mainloop()
